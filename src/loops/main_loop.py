83de3f80068159938ceeef92a8ee826311198bc5
Mr Chamster <s.duda03@gmail.com>
2025-08-04 19:17:41 +0200

    Wire up print and append loops

--- src/loops/main_loop.py
+++ src/loops/main_loop.py
@@ -1,5 +1,7 @@
 from os import chdir
 from src.askers.main_askers import ask_path_filedialog, ask_main_action
+from src.loops.print_loops import print_loop
+from src.loops.append_loops import append_loop
 
 
 
@@ -17,9 +19,13 @@ def main_loop():
         if action == None:
             return
         elif action == "print":
-            pass
+            outer = print_loop()
+            if outer == None:
+                return
         elif action == "append":
-            pass
+            outer = append_loop()
+            if outer == None:
+                return
         elif action == "change":
             dir_main = ask_path_filedialog("d", "Choose audio directory")
             if dir_main == "":
