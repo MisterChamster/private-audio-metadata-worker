9d2888bea700b58fa8ae9a92d1c89fb2047728a9
Mr Chamster <s.duda03@gmail.com>
2025-08-04 22:06:29 +0200

    Bug fix, comment old trash

--- src/loops/main_loop.py
+++ src/loops/main_loop.py
@@ -15,29 +15,30 @@ def main_loop():
         else:
             if path_type == "file":
                 main_path = ask_path_filedialog("f", "Choose mp3 or flac audio file")
+                if main_path == "":
+                    return
             elif path_type == "directory":
                 main_path = ask_path_filedialog("d", "Choose audio directory")
+                if main_path == "":
+                    return
+                chdir(main_path)
 
-            if main_path == "":
-                return
-            chdir(main_path)
 
-
-        print()
-        print(f"Current directory: {main_path}")
-        action = ask_main_action()
-        if action == None:
-            return
-        elif action == "print":
-            outer = print_loop()
-            if outer == None:
-                return
-        elif action == "append":
-            outer = append_loop()
-            if outer == None:
-                return
-        elif action == "change":
-            continue
+        # print()
+        # print(f"Current directory: {main_path}")
+        # action = ask_main_action()
+        # if action == None:
+        #     return
+        # elif action == "print":
+        #     outer = print_loop()
+        #     if outer == None:
+        #         return
+        # elif action == "append":
+        #     outer = append_loop()
+        #     if outer == None:
+        #         return
+        # elif action == "change":
+        #     continue
             # main_path = ask_path_filedialog("d", "Choose audio directory")
             # if main_path == "":
             #     return
