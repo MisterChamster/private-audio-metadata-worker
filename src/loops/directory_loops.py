2547127e6b886a46a0305d4fe329a21934ce3d1c
Mr Chamster <s.duda03@gmail.com>
2025-08-05 23:54:14 +0200

    Inside dir loops decl + wireup

--- src/loops/directory_loops.py
+++ src/loops/directory_loops.py
@@ -3,16 +3,31 @@ from os import chdir
 
 
 
+def print_loop():
+    return None
+
+
+def append_loop():
+    return None
+
+
 def directory_loop(dir_path: str):
     chdir(dir_path)
     while True:
         asker = ask_main_dir_action(dir_path)
         if asker == "print":
-            pass
+            outer = print_loop()
+            if outer == None:
+                return None
+
         elif asker == "append":
-            pass
+            outer = append_loop()
+            if outer == None:
+                return None
+
         elif asker == "change_path":
             return asker
+
         elif asker == None:
             return None
 
