a1b34e71a1505ef0e493d5ae7edf86f36868651d
Mr Chamster <s.duda03@gmail.com>
2025-08-05 10:56:04 +0200

    ask_main_dir_action def

--- src/askers/dir_askers.py
+++ src/askers/dir_askers.py
@@ -1,8 +1,20 @@
 def ask_main_dir_action(dir_path: str):
-    returns_dict = {}
+    returns_dict = {"pm": "print",
+                    "am": "append",
+                    "cd": "change_dir"}
 
     while True:
         print(f"Current directory path: {dir_path}\n" \
                "Choose action:\n"\
-               )
-        return None
+               "pm   - Print metadata...\n"\
+               "am   - Append metadata...\n"\
+               "cd   - Change path\n"\
+               "exit - Exit program\n>> ", end="")
+        asker = input()
+
+        if asker == "exit":
+            return None
+        elif asker in returns_dict:
+            return returns_dict[asker]
+        else:
+            print("Incorrect input.\n")
