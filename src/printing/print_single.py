9da4743f5399bd20d3250204ebc6a6c185221c4f
Mr Chamster <s.duda03@gmail.com>
2025-08-06 01:06:32 +0200

    Basic structure of mp3 printers

--- src/printing/print_single.py
+++ src/printing/print_single.py
@@ -1,10 +1,39 @@
+from mutagen.easyid3 import EasyID3
+
+
+
 def print_all_mp3(file_path: str):
-    return
+    try:
+        audio = EasyID3(file_path)
+    except Exception as e:
+        print(f"Reading metadata caused an error: {e}")
+        return
+    finally:
+        if audio is None:
+            print(f"Failed to load MP3 file: {file_path}")
+            return
+
 
 
 def print_appendable_mp3(file_path: str):
-    return
+    try:
+        audio = EasyID3(file_path)
+    except Exception as e:
+        print(f"Reading metadata caused an error: {e}")
+        return
+    finally:
+        if audio is None:
+            print(f"Failed to load MP3 file: {file_path}")
+            return
 
 
 def print_specific_mp3(file_path: str, md_name: str):
-    return
+    try:
+        audio = EasyID3(file_path)
+    except Exception as e:
+        print(f"Reading metadata caused an error: {e}")
+        return
+    finally:
+        if audio is None:
+            print(f"Failed to load MP3 file: {file_path}")
+            return
