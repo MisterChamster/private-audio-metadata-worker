7a559e4ebcbe42cd87e962a77c0af7adb33e7274
Mr Chamster <s.duda03@gmail.com>
2025-08-05 23:25:38 +0200

    File loop asker responses decl

--- src/loops/file_loops.py
+++ src/loops/file_loops.py
@@ -5,7 +5,15 @@ from src.askers.file_askers import ask_main_file_action
 def file_loop(file_path: str):
     while True:
         print(f"File path: {file_path}")
-        outer = ask_main_file_action(file_path)
-        if outer == None:
+        asker = ask_main_file_action(file_path)
+        if asker == "print":
+            pass
+        elif asker == "append":
+            pass
+        elif asker == "change_dir":
+            pass
+        elif asker == None:
             return None
+
+        # For testing
         return None
