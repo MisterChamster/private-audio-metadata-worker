6e435d25b0689dba50f4d146bb8d39467ad805a6
Mr Chamster <s.duda03@gmail.com>
2025-08-06 00:17:58 +0200

    ask_print_loop def

--- src/askers/dir_askers.py
+++ src/askers/dir_askers.py
@@ -21,8 +21,31 @@ def ask_main_dir_action(dir_path: str):
 
 
 def ask_print_loop():
-    return None
+    returns_dict = {"pa":  "print_all",
+                    "par": "print_all_recursive",
+                    "ps":  "print_specific",
+                    "psr": "print_specific_recursive",
+                    "rt":  "return"}
+
+    while True:
+        print("Choose printing option:\n"\
+              "pa   - Print metadata of all files\n"\
+              "par  - Print metadata of all files recursively\n"\
+              "ps   - Print specific metadata of all files\n"\
+              "psr  - Print specific metadata of all files recursively\n"\
+              "rt   - Return\n"\
+              "exit - Exit program\n\n>> ", end="")
+        asker = input()
+
+        if asker == "exit":
+            return None
+        elif asker in returns_dict:
+            return returns_dict[asker]
+        else:
+            print("Incorrect input\n\n")
 
 
 def ask_append_loop():
-    return None
+    while True:
+        print("Choose append option:\n")
+        return None
