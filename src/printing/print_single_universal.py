2e069344ce1690cef68df65044b48b29982b0c82
Mr Chamster <s.duda03@gmail.com>
2025-08-06 01:45:27 +0200

    Universal print functions skeleton

---
+++ src/printing/print_single_universal.py
@@ -0,0 +1,10 @@
+def print_all(file_path: str) -> None:
+    return
+
+
+def print_appendable(file_path: str) -> None:
+    return
+
+
+def print_specific(file_path: str, md_name: str) -> None:
+    return
