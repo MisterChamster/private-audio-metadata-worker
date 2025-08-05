ddb89948570dcf8b63031d4df18bd9cf1bba8c22
Mr Chamster <s.duda03@gmail.com>
2025-08-06 01:48:44 +0200

    Mp3 printers wireup

--- src/printing/print_single_universal.py
+++ src/printing/print_single_universal.py
@@ -1,10 +1,25 @@
+from src.printing.print_single_mp3 import (print_all_mp3,
+                                           print_appendable_mp3,
+                                           print_specific_mp3)
+
+
+
 def print_all(file_path: str) -> None:
-    return
+    if file_path.endswith(".mp3"):
+        print_all_mp3(file_path)
+    elif file_path.endswith(".flac"):
+        return
 
 
 def print_appendable(file_path: str) -> None:
-    return
+    if file_path.endswith(".mp3"):
+        print_appendable_mp3(file_path)
+    elif file_path.endswith(".flac"):
+        return
 
 
 def print_specific(file_path: str, md_name: str) -> None:
-    return
+    if file_path.endswith(".mp3"):
+        print_specific_mp3(file_path, md_name)
+    elif file_path.endswith(".flac"):
+        return
