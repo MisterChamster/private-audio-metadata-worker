287f388f7acfba861f3b9f475714fa065405cc6a
Mr Chamster <s.duda03@gmail.com>
2025-08-06 01:55:12 +0200

    Flac printers decl

---
+++ src/printing/print_single_flac.py
@@ -0,0 +1,14 @@
+from mutagen.flac import FLAC
+
+
+
+def print_all_flac(file_path: str) -> None:
+    return
+
+
+def print_appendable_flac(file_path: str) -> None:
+    return
+
+
+def print_specific_flac(file_path: str, md_name: str) -> None:
+    return
