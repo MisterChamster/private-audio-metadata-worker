7be2d60c4f925d07e4d05d2bb082e1b8186c8e25
Mr Chamster <s.duda03@gmail.com>
2025-08-07 00:58:17 +0200

    Name change

--- src/file_ops/utils.py
+++ src/file_ops/utils.py
@@ -7,9 +7,9 @@ def get_audios_from_dir(dir_path: str) -> list:
     os.chdir(dir_path)
 
     valid_exts = ["mp3", "flac"]
-    audios_in_cwd = []
+    audios_in_dir = []
     for node in os.listdir():
         if node.split(".")[-1] in valid_exts:
-            audios_in_cwd.append(node)
+            audios_in_dir.append(node)
     os.chdir(og_path)
-    return audios_in_cwd
+    return audios_in_dir
