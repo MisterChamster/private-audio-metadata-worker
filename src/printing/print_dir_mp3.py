4274140cda348e55c699a2d19ced1c6c23167005
Mr Chamster <s.duda03@gmail.com>
2025-08-06 13:19:32 +0200

    print_dir_spec def + wireup

--- src/printing/print_dir_mp3.py
+++ src/printing/print_dir_mp3.py
@@ -23,4 +23,8 @@ def print_dir_appendable_metadata_mp3(dir_path: str) -> None:
 
 
 def print_dir_specific_metadata_mp3(dir_path: str, md_name: str) -> None:
-    return
+    files_list = get_audios_from_cwd(dir_path)
+    for filename in files_list:
+        file_path = Path(dir_path) / filename
+        print_specific_metadata_mp3(file_path, md_name)
+        print()
