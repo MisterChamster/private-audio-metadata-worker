3ff9ab7ad9c0e6103b067ef191ed0753bfaa8af1
Mr Chamster <s.duda03@gmail.com>
2025-08-07 00:16:51 +0200

    Reshape into class

---
+++ src/printing/PrintDirRecursive.py
@@ -0,0 +1,15 @@
+class PrintDirRecursive:
+    def __init__(self, dir_path: str, md_name: str):
+        self.dir_path = dir_path
+        self.md_name = md_name
+
+    def print_all_metadata_dir_recur() -> None:
+        return
+
+
+    def print_appendable_metadata_dir_recur() -> None:
+        return
+
+
+    def print_specific_metadata_dir_recur() -> None:
+        return
