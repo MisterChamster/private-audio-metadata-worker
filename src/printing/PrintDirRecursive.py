431ba0bb3ed6bd07f8d10fc280cf9c6e13e5d47a
Mr Chamster <s.duda03@gmail.com>
2025-08-07 00:34:22 +0200

    Final class structure

--- src/printing/PrintDirRecursive.py
+++ src/printing/PrintDirRecursive.py
@@ -5,8 +5,7 @@ import os
 # Okay, maybe id didn't have to be made with a class,
 # but it's a good practise nonetheless
 class PrintDirRecursive:
-    def __init__(self, dir_path: str, md_name: str):
-        self.dir_path = dir_path
+    def __init__(self, md_name: str):
         self.md_name = md_name
 
 
@@ -23,17 +22,20 @@ class PrintDirRecursive:
         return
 
 
-    def print_all_metadata_dir_recur(self, dir_path) -> None:
-        self.dir_path = dir_path
+    def print_all_metadata_dir_recur(self) -> None:
+        og_path = os.getcwd()
         self.recurrer_all()
+        os.chdir(og_path)
 
 
-    def print_appendable_metadata_dir_recur(self, dir_path) -> None:
-        self.dir_path = dir_path
+    def print_appendable_metadata_dir_recur(self) -> None:
+        og_path = os.getcwd()
         self.recurrer_appendable()
+        os.chdir(og_path)
 
 
-    def print_specific_metadata_dir_recur(self, dir_path, md_name) -> None:
-        self.dir_path = dir_path
+    def print_specific_metadata_dir_recur(self, md_name) -> None:
+        og_path = os.getcwd()
         self.md_name = md_name
         self.recurrer_specific()
+        os.chdir(og_path)
