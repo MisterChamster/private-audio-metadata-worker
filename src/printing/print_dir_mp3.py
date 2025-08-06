784f045a81a4a40c864d480b837adc769934bc05
Mr Chamster <s.duda03@gmail.com>
2025-08-06 11:23:50 +0200

    print_all_mp3 print names

--- src/printing/print_dir_mp3.py
+++ src/printing/print_dir_mp3.py
@@ -1,5 +1,11 @@
+from src.file_ops.utils import get_audios_from_cwd
+
+
+
 def print_all_mp3(dir_path: str) -> None:
-    return
+    files_list = get_audios_from_cwd(dir_path)
+    for filename in files_list:
+        print(filename)
 
 
 def print_appendable_mp3(dir_path: str) -> None:
