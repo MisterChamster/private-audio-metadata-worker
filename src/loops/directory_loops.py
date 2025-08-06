4b08ababea3aef3acd8a8af18fb1decb2d9c14d2
Mr Chamster <s.duda03@gmail.com>
2025-08-06 11:19:56 +0200

    print_all_mp3 wireup

--- src/loops/directory_loops.py
+++ src/loops/directory_loops.py
@@ -12,7 +12,7 @@ def print_loop(dir_path: str):
     while True:
         asker = ask_print_loop()
         if asker == "print_all":
-            pass
+            print_all_mp3(dir_path)
         elif asker == "print_all_recursive":
             pass
         elif asker == "print_appendable":
@@ -37,6 +37,7 @@ def directory_loop(dir_path: str):
     chdir(dir_path)
     while True:
         asker = ask_main_dir_action(dir_path)
+        print()
         if asker == "print":
             outer = print_loop(dir_path)
             if outer == None:
