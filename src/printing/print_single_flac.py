5442b5234df95bdd3b74a70cbae3841ce8dcce66
Mr Chamster <s.duda03@gmail.com>
2025-08-06 02:01:21 +0200

    Flac printers def

--- src/printing/print_single_flac.py
+++ src/printing/print_single_flac.py
@@ -3,12 +3,61 @@ from mutagen.flac import FLAC
 
 
 def print_all_flac(file_path: str) -> None:
-    return
+    try:
+        audio = FLAC(file_path)
+
+    except Exception as e:
+        print(f"Reading metadata caused an error: {e}")
+        return
+
+    if audio is None:
+        print(f"Failed to load FLAC file: {file_path}")
+        return
+
+    print()
+    max_len = len('tracknumber')
+    for key in audio:
+        addstr = (max_len-len(key)) * " "
+        print(f"{key}:{addstr} {audio[key]}")
 
 
 def print_appendable_flac(file_path: str) -> None:
-    return
+    try:
+        audio = FLAC(file_path)
+
+    except Exception as e:
+        print(f"Reading metadata caused an error: {e}")
+        return
+
+    if audio is None:
+        print(f"Failed to load FLAC file: {file_path}")
+        return
+
+    print()
+    specific_md = ['album', 'title', 'artist', 'tracknumber', 'date']
+    max_len = len('tracknumber')
+    for key in specific_md:
+        addstr = (max_len-len(key)) * " "
+        if key in audio:
+            print(f"{key}:{addstr} {audio[key]}")
+        else:
+            print(f"{key}:{addstr} Unavailable")
 
 
 def print_specific_flac(file_path: str, md_name: str) -> None:
-    return
+    try:
+        audio = FLAC(file_path)
+
+    except Exception as e:
+        print(f"Reading metadata caused an error: {e}")
+        return
+
+    if audio is None:
+        print(f"Failed to load FLAC file: {file_path}")
+        return
+
+    print()
+    if md_name in audio:
+        print(f"{md_name}: {audio[md_name]}")
+    else:
+        print(f"{md_name}: Unavailable")
