62829a8af52440e135abe30f24be707db1cf4160
Mr Chamster <s.duda03@gmail.com>
2025-08-07 12:01:16 +0200

    Specific merge to one

--- src/printing/print_single_universal.py
+++ src/printing/print_single_universal.py
@@ -52,7 +52,21 @@ def print_appendable_metadata_single(file_path: str) -> None:
 
 
 def print_specific_metadata_single(file_path: str, md_name: str) -> None:
-    if file_path.endswith(".mp3"):
-        print_specific_metadata_mp3(file_path, md_name)
-    elif file_path.endswith(".flac"):
-        print_specific_metadata_flac(file_path, md_name)
+    try:
+        if file_path.endswith(".mp3"):
+            audio = EasyID3(file_path)
+        elif file_path.endswith(".flac"):
+            audio = FLAC(file_path)
+
+    except Exception as e:
+        print(f"Reading metadata caused an error: {e}")
+        return
+
+    if audio is None:
+        print(f"Failed to load file: {file_path}")
+        return
+
+    if md_name in audio:
+        print(f"{md_name}: {audio[md_name]}")
+    else:
+        print(f"{md_name}: Unavailable")
