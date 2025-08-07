6e20e23bda5c311887684d53cc7a7cc3da7f4a21
Mr Chamster <s.duda03@gmail.com>
2025-08-07 11:49:09 +0200

    print_all_metadata_single merge to one

--- src/printing/print_single_universal.py
+++ src/printing/print_single_universal.py
@@ -1,18 +1,31 @@
-from src.printing.print_single_mp3 import (print_all_metadata_mp3,
-                                           print_appendable_metadata_mp3,
+from src.printing.print_single_mp3 import (print_appendable_metadata_mp3,
                                            print_specific_metadata_mp3)
-from src.printing.print_single_flac import (print_all_metadata_flac,
-                                            print_appendable_metadata_flac,
+from src.printing.print_single_flac import (print_appendable_metadata_flac,
                                             print_specific_metadata_flac)
-
+from mutagen.easyid3 import EasyID3
+from mutagen.flac import FLAC
 
 
 
 def print_all_metadata_single(file_path: str) -> None:
-    if file_path.endswith(".mp3"):
-        print_all_metadata_mp3(file_path)
-    elif file_path.endswith(".flac"):
-        print_all_metadata_flac(file_path)
+    try:
+        if file_path.endswith(".mp3"):
+            audio = EasyID3(file_path)
+        elif file_path.endswith(".flac"):
+            audio = FLAC(file_path)
+
+    except Exception as e:
+        print(f"Reading metadata caused an error: {e}")
+        return
+
+    if audio is None:
+        print(f"Failed to load file: {file_path}")
+        return
+
+    max_len = len('tracknumber')
+    for key in audio:
+        addstr = (max_len-len(key)) * " "
+        print(f"{key}:{addstr} {audio[key]}")
 
 
 def print_appendable_metadata_single(file_path: str) -> None:
