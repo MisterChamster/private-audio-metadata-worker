2937a3a39ad40b4f7bf39555caa98a60fd82a3b8
Mr Chamster <s.duda03@gmail.com>
2025-08-07 22:35:37 +0200

    append_metadata_dir def with error

--- src/appending/append_dir_universal.py
+++ src/appending/append_dir_universal.py
@@ -1,2 +1,13 @@
+from src.file_ops.utils import get_audios_from_dir
+from pathlib import Path
+from src.appending.append_single_universal import append_file_metadata_universal
+
+
+
 def append_metadata_dir(dir_path: str, md_type: str, md_text: str):
-    return
+    files_list = get_audios_from_dir(dir_path)
+    for filename in files_list:
+        print(filename)
+        file_path = Path(dir_path) / filename
+        append_file_metadata_universal(file_path, md_type, md_text)
+        print()
