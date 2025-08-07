608d78344fbe50d7cb2347877d6d90c4486b7140
Mr Chamster <s.duda03@gmail.com>
2025-08-07 11:18:12 +0200

    Name change, universal functionality

--- src/appending/append_single_mp3.py
+++
@@ -1,8 +0,0 @@
-from mutagen.easyid3 import EasyID3
-
-
-
-def append_file_metadata_mp3(file_path: str, md_type: str, md_text: str):
-    audio_file = EasyID3(file_path)
-    audio_file[md_type] = md_text
-    audio_file.save()
