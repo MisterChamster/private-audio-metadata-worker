951937abe746b253b7683c1929c0337b935a20a7
Mr Chamster <s.duda03@gmail.com>
2025-08-07 11:16:04 +0200

    append_file_metadata_mp3 def

--- src/appending/append_single_mp3.py
+++ src/appending/append_single_mp3.py
@@ -1,2 +1,8 @@
+from mutagen.easyid3 import EasyID3
+
+
+
 def append_file_metadata_mp3(file_path: str, md_type: str, md_text: str):
-    return
+    audio_file = EasyID3(file_path)
+    audio_file[md_type] = md_text
+    audio_file.save()
