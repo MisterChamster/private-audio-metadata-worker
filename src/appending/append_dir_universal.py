07f8378514ab32d8fd73d0d5e8b8f366b5a8e459
Mr Chamster <s.duda03@gmail.com>
2025-08-08 01:28:29 +0200

    append_tracknum_dir safe get nums list

--- src/appending/append_dir_universal.py
+++ src/appending/append_dir_universal.py
@@ -1,6 +1,7 @@
 from src.file_ops.utils import get_audios_from_dir
 from pathlib import Path
 from src.appending.append_single_universal import append_metadata_file_universal
+from src.utils import get_tracknumber
 
 
 
@@ -15,4 +16,15 @@ def append_metadata_dir(dir_path: str, md_type: str, md_text: str):
 
 def append_tracknum_dir(dir_path: str):
     files_list = get_audios_from_dir(dir_path)
+    tracknums_list = []
+    for filename in files_list:
+        try:
+            tracknums_list.append(get_tracknumber(filename))
+        except Exception as e:
+            print(f"Getting track from {filename} number caused an error: {e}")
+            tracknums_list.append(None)
+
+    for i in range(len(files_list)):
+        print(f"Number: {tracknums_list[i]} for track: {files_list[i]}")
+
     #write more stuff here
