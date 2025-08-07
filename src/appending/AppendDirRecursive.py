4f7fb58b1a8cd1b270f462d353941a8c46ef1d3c
Mr Chamster <s.duda03@gmail.com>
2025-08-07 23:27:28 +0200

    _recurrer method decl adn wireup

--- src/appending/AppendDirRecursive.py
+++ src/appending/AppendDirRecursive.py
@@ -1,4 +1,18 @@
+import os
+
+
+
 class AppendDirRecursive:
-    def __init__(self, md_name: str, md_text: str):
+    def __init__(self, md_name: str = None, md_text: str = None):
         self.md_name = md_name
         self.md_text = md_text
+
+
+    def _recurrer(self, dir_path: str):
+        return
+
+
+    def append_metadata_dir_recur(self, dir_path: str, md_name: str, md_text: str):
+        og_path = os.getcwd()
+        self._recurrer(dir_path)
+        os.chdir(og_path)
