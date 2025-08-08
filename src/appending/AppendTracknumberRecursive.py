ef3d6bfec7381b8c41ff289c5ca439e0e7038f78
Mr Chamster <s.duda03@gmail.com>
2025-08-08 12:52:42 +0200

    recurrer def

--- src/appending/AppendTracknumberRecursive.py
+++ src/appending/AppendTracknumberRecursive.py
@@ -1,10 +1,23 @@
 import os
+from pathlib import Path
+from src.appending.append_dir_universal import append_tracknum_dir
+from src.file_ops.utils import get_dirs_from_dir
 
 
 
 class AppendTracknumberRecursive:
     def _recurrer(self, dir_path: str):
-        return
+        os.chdir(dir_path)
+        print(f"Directory name: {os.path.basename(dir_path)}")
+        print()
+        append_tracknum_dir(dir_path)
+
+        dirs_list = get_dirs_from_dir(dir_path)
+        for dir_name in dirs_list:
+            full_path = str(Path(dir_path) / dir_name)
+            self._recurrer(full_path)
+        os.chdir("..")
+
 
     def append_metadata_dir_recur(self, dir_path: str):
         og_path = os.getcwd()
