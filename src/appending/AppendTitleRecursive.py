a836eecd9c7f1681933bf68a555fbf8bebdf5a77
Mr Chamster <s.duda03@gmail.com>
2025-08-08 19:53:19 +0200

    Class full def

--- src/appending/AppendTitleRecursive.py
+++ src/appending/AppendTitleRecursive.py
@@ -8,7 +8,17 @@ from src.file_ops.utils import (get_dirs_from_dir,
 
 class AppendTitleRecursive:
     def _recurrer(self, dir_path: str):
-        return
+        os.chdir(dir_path)
+        print(f"Directory name: {os.path.basename(dir_path)}")
+        print()
+        if is_audio_in_dir(dir_path):
+            append_title_dir(dir_path)
+
+        dirs_list = get_dirs_from_dir(dir_path)
+        for dir_name in dirs_list:
+            full_path = str(Path(dir_path) / dir_name)
+            self._recurrer(full_path)
+        os.chdir("..")
 
     def append_title_dir_recur(self, dir_path: str):
         og_path = os.getcwd()
