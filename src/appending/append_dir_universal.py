6293fb93be930aa3fd6f09b6e5b03b7651373c99
Mr Chamster <s.duda03@gmail.com>
2025-08-10 00:22:05 +0200

    UI update

--- src/appending/append_dir_universal.py
+++ src/appending/append_dir_universal.py
@@ -111,7 +111,7 @@ def append_date_dir(dir_path: str):
             date_text = outer
 
     if confirm_block == True:
-        print(f"Append date {date_text} to audio files in folder {os.path.basename(dir_path)}?")
+        print(f"Append date '{date_text}' to audio files in folder {os.path.basename(dir_path)}?")
         outer = ask_date_action()
         if outer != "accept":
             date_text = outer
@@ -139,7 +139,7 @@ def append_album_dir(dir_path: str, del_until: str):
             album_text = outer
 
     if confirm_block == True:
-        print(f"Append album name {album_text} to audio files in folder {os.path.basename(dir_path)}?")
+        print(f"Append album name '{album_text}' to audio files in folder {os.path.basename(dir_path)}?")
         outer = ask_album_action()
         if outer != "accept":
             album_text = outer
