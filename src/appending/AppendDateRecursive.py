d4f5a77c51149c963da56d7d89e39053eab9a114
MisterChamster <s.duda03@gmail.com>
2025-08-09 13:13:51 +0200

    recurrer def cont

--- src/appending/AppendDateRecursive.py
+++ src/appending/AppendDateRecursive.py
@@ -1,10 +1,25 @@
 import os
+from pathlib import Path
+from src.file_ops.utils import (get_dirs_from_dir,
+                                is_audio_in_dir)
 
 
 
 class AppendDateRecursive:
-    def _recurrer(_self, dir_path: str):
-        return
+    def _recurrer(self, dir_path: str):
+        os.chdir(dir_path)
+        dirname = os.path.basename(dir_path)
+        print(f"Directory name: {dirname}")
+        print()
+        if is_audio_in_dir(dir_path):
+            # Get back HERE
+            pass
+
+        dirs_list = get_dirs_from_dir(dir_path)
+        for dir_name in dirs_list:
+            full_path = str(Path(dir_path) / dir_name)
+            self._recurrer(full_path)
+        os.chdir("..")
 
     def append_date_dir_recur(self, dir_path: str):
         og_path = os.getcwd()
