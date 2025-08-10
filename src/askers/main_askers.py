829e1c0027381ee41160bcf875bbabbb68437c3a
MisterChamster <s.duda03@gmail.com>
2025-08-10 17:04:30 +0200

    ask_path_filedialog update

--- src/askers/main_askers.py
+++ src/askers/main_askers.py
@@ -27,18 +27,21 @@ def ask_path_filedialog(type: str, message: str):
     desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
     os.chdir(desktop_path)
 
-    sel_path = ""
+    selected_path = ""
     if type == "f":
         while True:
-            sel_path = filedialog.askopenfilename(title=message)
-            if (sel_path.endswith("mp3")) or \
-               (sel_path.endswith("flac")) or \
-               (sel_path == ""):
+            selected_path = filedialog.askopenfilename(title=message)
+            filename = os.path.basename(selected_path)
+
+            if ((selected_path.endswith("mp3")) or \
+                (selected_path.endswith("flac")) or \
+                (selected_path == "")) and \
+                (not filename.startswith(".")):
                 break
             else:
                 print("Incorrect file extension.")
     elif type == "d":
-        sel_path = filedialog.askdirectory(title=message)
+        selected_path = filedialog.askdirectory(title=message)
 
     os.chdir(original_path)
-    return sel_path
+    return selected_path
