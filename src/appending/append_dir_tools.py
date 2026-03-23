fecdf30339ce6a6eb59e006ef0ceffe30d032285
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-03-23 20:57:30 +0100

    Conv to Path

--- src/appending/append_dir_tools.py
+++ src/appending/append_dir_tools.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import os
 
 import src.utils_file_ops as utils_file
 from src.appending.append_single_tools import append_metadata_file_universal
@@ -8,33 +7,35 @@ import src.askers.askers_appending as ask_append
 
 
 
-def append_metadata_dir(dir_path: Path, md_type: str, md_text: str) -> None:
-    # TEMPPPPPP idk
-    files_list = utils_file.get_audios_from_dir(dir_path)
-    files_list = [str(a.name) for a in files_list]
-    for filename in files_list:
-        print(filename)
-        file_path = dir_path / filename
+def append_metadata_dir(
+    dir_path: Path,
+    md_type: str,
+    md_text: str
+) -> None:
+    files_paths = utils_file.get_audios_from_dir(dir_path)
+    for file_path in files_paths:
+        print(file_path.name)
         append_metadata_file_universal(file_path, md_type, md_text)
     print()
 
 
 def append_tracknum_dir(dir_path: Path) -> None:
-    # TEMPPPPPP idk
-    files_list = utils_file.get_audios_from_dir(dir_path)
-    files_list = [str(a.name) for a in files_list]
+    files_paths = utils_file.get_audios_from_dir(dir_path)
     tracknums_list = []
 
-    for filename in files_list:
+    for file_path in files_paths:
+        filename = file_path.name
         try:
             tracknums_list.append(utils_common.get_tracknumber(filename))
         except Exception as e:
             print(f"Getting track number from {filename} caused an error: {e}")
             tracknums_list.append(None)
 
-    for i in range(len(files_list)):
-        print_num = tracknums_list[i] if tracknums_list[i] is not None else "None"
-        print(f"Number: {print_num:<2} for: {files_list[i]}")
+    for i in range(len(files_paths)):
+        print_num = (tracknums_list[i]
+                     if tracknums_list[i] is not None
+                     else "None")
+        print(f"Number: {print_num:<2} for: {files_paths[i].name}")
     print()
 
     outer = ask_append.ask_accept_tracknum()
@@ -42,38 +43,43 @@ def append_tracknum_dir(dir_path: Path) -> None:
     if outer == "decline":
         return
     elif outer == "alphabetical":
-        for i in range(len(files_list)):
-            file_path = dir_path / files_list[i]
-            append_metadata_file_universal(file_path, "tracknumber", str(i+1))
+        for i, file_path in enumerate(files_paths):
+            append_metadata_file_universal(
+                file_path,
+                "tracknumber",
+                str(i+1))
         return
 
-    for i in range(len(files_list)):
+    for i, file_path in enumerate(files_paths):
         if tracknums_list[i] is not None:
-            file_path = dir_path / files_list[i]
-            append_metadata_file_universal(file_path, "tracknumber", tracknums_list[i])
+            append_metadata_file_universal(
+                file_path,
+                "tracknumber",
+                tracknums_list[i])
 
 
 def append_title_dir(dir_path: Path, del_until: str) -> None:
-    # TEMPPPPPPP sth
-    files_list = utils_file.get_audios_from_dir(dir_path)
-    files_list = [str(a.name) for a in files_list]
+    files_paths = utils_file.get_audios_from_dir(dir_path)
     titles_list = []
 
-    for filename in files_list:
+    for file_path in files_paths:
+        file_name = file_path.name
         try:
-            titles_list.append(utils_common.get_song_title(filename, del_until))
+            titles_list.append(utils_common.get_song_title(file_name, del_until))
         except Exception as e:
-            print(f"Getting title from {filename} caused an error: {e}")
+            print(f"Getting title from {file_name} caused an error: {e}")
             titles_list.append(None)
 
     while True:
-        for i in range(len(files_list)):
+        for i, file_path in enumerate(files_paths):
             print_title = f"'{titles_list[i]}'" if titles_list[i] is not None else "None"
             title_with_quotation = print_title
-            print(f"Nr: {str(i+1):<2} Title: {title_with_quotation:<30} for: {files_list[i]}")
+            print(f"Nr: {str(i+1):<2} "
+                  f"Title: {title_with_quotation:<30} "
+                  f"for: {file_path.name}")
         print()
 
-        outer = ask_append.ask_accept_or_change_name(len(files_list))
+        outer = ask_append.ask_accept_or_change_name(len(files_paths))
         print()
         if outer == "false":
             print("\n")
@@ -87,16 +93,15 @@ def append_title_dir(dir_path: Path, del_until: str) -> None:
             titles_list[new_title_index] = new_title
             print("\n\n")
 
-    for i in range(len(files_list)):
+    for i, file_path in enumerate(files_paths):
         if titles_list[i] is not None:
-            file_path = dir_path / files_list[i]
             append_metadata_file_universal(file_path, "title", titles_list[i])
 
 
 def append_date_dir(dir_path: Path) -> None:
     # TEMPPPPPPP idkk
-    files_list = utils_file.get_audios_from_dir(dir_path)
-    files_list = [str(a.name) for a in files_list]
+    files_paths = utils_file.get_audios_from_dir(dir_path)
+    files_names = [str(a.name) for a in files_paths]
     date_text = ""
     confirm_block = False
 
@@ -121,15 +126,15 @@ def append_date_dir(dir_path: Path) -> None:
         if asker != "accept":
             date_text = asker
 
-    for i in range(len(files_list)):
-        file_path = dir_path / files_list[i]
+    for i in range(len(files_names)):
+        file_path = dir_path / files_names[i]
         append_metadata_file_universal(file_path, "date", date_text)
 
 
 def append_album_dir(dir_path: Path, del_until: str) -> None:
     # TEMPPPPPPP sthh
-    files_list = utils_file.get_audios_from_dir(dir_path)
-    files_list = [str(a.name) for a in files_list]
+    files_paths = utils_file.get_audios_from_dir(dir_path)
+    files_names = [str(a.name) for a in files_paths]
     album_text = ""
     confirm_block = False
 
@@ -154,6 +159,6 @@ def append_album_dir(dir_path: Path, del_until: str) -> None:
         elif outer != "accept":
             album_text = outer
 
-    for i in range(len(files_list)):
-        file_path = dir_path / files_list[i]
+    for i in range(len(files_names)):
+        file_path = dir_path / files_names[i]
         append_metadata_file_universal(file_path, "album", album_text)
