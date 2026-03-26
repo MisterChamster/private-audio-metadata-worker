6926c65af118029c3e198368adea40fde5356881
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-03-26 23:57:07 +0100

    Name change

--- src/md_printers/print_file_tools.py
+++ src/md_printers/print_file_tools.py
@@ -9,41 +9,43 @@ def print_all_metadata_file(file_path: Path) -> None:
     md_keys = audio.keys()
 
     appendable_md = ['album', 'title', 'artist', 'tracknumber', 'date']
-    present_not_appentable = []
-    present_appentable = []
+    present_not_appendable = []
+    present_appendable = []
     for md in md_keys:
         if md in appendable_md:
-            present_appentable.append(md)
+            present_appendable.append(md)
         else:
-            present_not_appentable.append(md)
+            present_not_appendable.append(md)
 
-    if len(present_not_appentable) > 0:
-        present_not_appentable.sort()
+    # Print non-appendable keys
+    if len(present_not_appendable) > 0:
+        present_not_appendable.sort()
         max_len_no_app = 1 + max(
             len(string)
             for string
-            in present_not_appentable)
+            in present_not_appendable)
         padded_keys_no_app = [
             (string + ":").ljust(max_len_no_app)
             for string
-            in present_not_appentable]
+            in present_not_appendable]
 
-        for i, key in enumerate(present_not_appentable):
+        for i, key in enumerate(present_not_appendable):
             print(f"{padded_keys_no_app[i]} '{audio[key][0]}'")
 
+    # Print appendable keys
     print()
-    if len(present_appentable) > 0:
-        present_appentable.sort()
+    if len(present_appendable) > 0:
+        present_appendable.sort()
         max_len_app = 1 + max(
             len(string)
             for string
-            in present_appentable)
+            in present_appendable)
         padded_keys_app = [
             (string + ":").ljust(max_len_app)
             for string
-            in present_appentable]
+            in present_appendable]
 
-        for i, key in enumerate(present_appentable):
+        for i, key in enumerate(present_appendable):
             print(f"{padded_keys_app[i]} '{audio[key][0]}'")
 
 
