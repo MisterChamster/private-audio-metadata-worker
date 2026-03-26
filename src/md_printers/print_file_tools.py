533dedc306a69a8974b4e98fd2de19168286e3c2
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-03-26 22:42:40 +0100

    print_all_metadata_file upgrade

--- src/md_printers/print_file_tools.py
+++ src/md_printers/print_file_tools.py
@@ -6,19 +6,53 @@ import src.utils_file_ops as file_ops
 
 def print_all_metadata_file(file_path: Path) -> None:
     audio = file_ops.get_audio(file_path)
+    md_keys = audio.keys()
 
-    max_len = len('tracknumber')
-    for key in audio:
-        addstr = (max_len-len(key)) * " "
-        print(f"{key}:{addstr} '{audio[key][0]}'")
+    appendable_md = ['album', 'title', 'artist', 'tracknumber', 'date']
+    present_not_appentable = []
+    present_appentable = []
+    for md in md_keys:
+        if md in appendable_md:
+            present_appentable.append(md)
+        else:
+            present_not_appentable.append(md)
+
+    if len(present_not_appentable) > 0:
+        present_not_appentable.sort()
+        max_len_no_app = 1 + max(
+            len(string)
+            for string
+            in present_not_appentable)
+        padded_keys_no_app = [
+            (string + ":").ljust(max_len_no_app)
+            for string
+            in present_not_appentable]
+
+        for i, key in enumerate(present_not_appentable):
+            print(f"{padded_keys_no_app[i]} '{audio[key][0]}'")
+
+    print()
+    if len(present_appentable) > 0:
+        present_appentable.sort()
+        max_len_app = 1 + max(
+            len(string)
+            for string
+            in present_appentable)
+        padded_keys_app = [
+            (string + ":").ljust(max_len_app)
+            for string
+            in present_appentable]
+
+        for i, key in enumerate(present_appentable):
+            print(f"{padded_keys_app[i]} '{audio[key][0]}'")
 
 
 def print_appendable_metadata_file(file_path: Path) -> None:
     audio = file_ops.get_audio(file_path)
 
-    specific_md = ['album', 'title', 'artist', 'tracknumber', 'date']
+    appendable_md = ['album', 'title', 'artist', 'tracknumber', 'date']
     max_len = len('tracknumber')
-    for key in specific_md:
+    for key in appendable_md:
         addstr = (max_len-len(key)) * " "
         if key in audio:
             print(f"{key}:{addstr} '{audio[key][0]}'")
