2cb01eb959d6d4f43e63ca1936c6b464bc0b184d
Szymon Duda <s.duda03@gmail.com>
2026-07-05 14:00:22 +0200

    ask_removal_loop

---
+++ src/askers/askers_removal.py
@@ -0,0 +1,29 @@
+from typing import Literal
+
+
+
+def ask_removal_loop() -> Literal[
+    "exit",
+    "return"]:
+
+    returns_dict = {
+        "a": "all",
+        "p": "appendable",
+        "s": "specific",
+        "r": "return",
+        "x": "exit"}
+
+    while True:
+        print("Choose a removal option:\n"
+              "a - All metadata\n"
+              "p - Appendable metadata\n"
+              "s - Specific metadata\n"
+              "r - Return\n"
+              "x - Exit program\n>> ", end="")
+        asker = input().strip().lower()
+
+        if asker in returns_dict:
+            return returns_dict[asker]
+        else:
+            print("Incorrect input\n\n")
+
--- src/loops/file_loops.py
+++ src/loops/file_loops.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import src.askers.askers_utils      as ask_utils
 import src.askers.askers_main_menus as ask_main
+import src.askers.askers_removal    as ask_removal
 import src.md_printers.print_file_tools  as printers
 import src.appending.append_single_tools as appenders
 
@@ -32,6 +33,23 @@ def append_loop(file_path: Path) -> bool:
             print("\n\n")
 
 
+def removal_loop(file_path: Path) -> bool:
+    exit_flags = {
+        "return": False,
+        "exit": True}
+
+    while True:
+        removal_type = ask_removal.ask_removal_loop()
+        print("\n")
+
+        exit_flags = {"return": False, "exit": True}
+        if removal_type in exit_flags:
+            return exit_flags[removal_type]
+
+        print("Work in progress")
+
+
+
 def file_loop(file_path: Path) -> bool:
     while True:
         asker = ask_main.ask_main_file_action(file_path)
@@ -50,8 +68,9 @@ def file_loop(file_path: Path) -> bool:
                 return True
 
         elif asker == "remove":
-            print("Work in progress")
-            pass
+            exit_flag = removal_loop(file_path)
+            if exit_flag == True:
+                return True
 
         else:
             exit_flags = {
