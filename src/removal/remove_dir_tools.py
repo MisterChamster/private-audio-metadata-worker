75245db700afbdcf8b7b1fd6758334f0e55fb4e3
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-07-07 15:54:24 +0200

    Removers dir

---
+++ src/removal/remove_dir_tools.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+import src.removal.remove_single_tools as removers
+
+
+
+def remove_all_md_dir(dir_path: Path) -> None:
+    for file_path in dir_path.iterdir():
+        removers.IDKFUNC(file_path)
